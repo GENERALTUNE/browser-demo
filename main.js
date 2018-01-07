ebba08477f28ce98aa526edb1528015e2bd88fc1
GENERALTUNE <577174042@qq.com>
2018-01-07 16:28:28 +0800

    初始化项目

---
+++ main.js
@@ -0,0 +1,21 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+let win
+
+app.on('ready', () => {
+
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadURL(`file:///${__dirname}/index.html`)
+
+    win.on('closed', () => {
+        win = null
+    })
+
+})
