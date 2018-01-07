5aecb91cfa347488e7145995a6860d501429dcb2
GENERALTUNE <577174042@qq.com>
2018-01-07 17:58:22 +0800

    调整样式

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ let win
 app.on('ready', () => {
 
     win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+        width: 1260,
+        height: 800,
+        title: "乐彩网浏览器",
+        icon: 'images/logo.gif'
+        // frame: false
+    });
+
+    //隐藏菜单栏
+    win.setMenu(null);
 
     win.loadURL(`file:///${__dirname}/index.html`)
 
