75234188d763dc90ef83290bd74dfc8a5ab69f7b
GENERALTUNE <577174042@qq.com>
2018-02-04 16:51:44 +0800

    支持flash 插件

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ const {
 
 let win
 
+
+//此处仅兼容了Windows 的 32位 与 64位
+const pepflashplayer = (process.arch == 'x64')? `${__dirname}/flash/pepflashplayer64_23_0_0_207.dll`:`${__dirname}/flash/pepflashplayer32_23_0_0_207.dll`;
+//设定插件
+app.commandLine.appendSwitch('ppapi-flash-path', pepflashplayer);
+//设定插件版本（不知道是否真有用，不匹配貌似也能运行）
+app.commandLine.appendSwitch('ppapi-flash-version', '23.0.0.207');
+
 app.on('ready', () => {
 
     win = new BrowserWindow({
